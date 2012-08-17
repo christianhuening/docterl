022f6315240aad6a610df307fdd68362e10c7dc6
Lutz Behnke <lutz.behnke@gmx.de>
2012-08-17 07:53:50 +0200

    WIP: clean up and complete API tests

--- apps/docterl_ets/test/docterl_ets_tests.erl
+++ apps/docterl_ets/test/docterl_ets_tests.erl
@@ -9,31 +9,37 @@ info_test_() ->
     { setup, fun() -> ok end, 
       fun() -> ?debugFmt("~n############################################~n      starting ~p~n############################################~n  ", [?MODULE]) end }.
 
+basic_api_test_() ->
+    { "test the api functions of the docter_ets module",
+      setup,
+      fun() -> 
+              %%  application:start(sasl),
+              doe_ets:start_link(),
+              doe_event_mgr:start_link(),    
+              ok
+      end,
+      fun(_Args) ->
+              %%  application:stop(sasl),
+              doe_ets:stop(),
+              doe_event_mgr:stop(),
+              sleep(100),
+              ok
+      end,
+      fun(_Args) -> [?_test(test_new_tree()),
+                     ?_test(test_new_obj())] 
+              end
+      }.
 
-new_tree_test_() -> 
-    { "create a new tree",
-      { setup,
-        fun fixStart/0,
-        fun fixStop/1,
-        fun(_Foo) -> [
-                      ?_test(
-                      begin
-                          {ok, TreeId} = docterl_ets:new_tree([])
-                      end)
-                     ] end }}.
+test_new_tree() -> 
+    ?assert({ok, _TreeId} = docterl_ets:new_tree([])).
 
-new_obj_test_() -> 
-    { "create a new obj",
-      { setup,
-        fun fixStart/0,
-        fun fixStop/1,
-        fun(_Foo) -> [
-                      ?_test(
-                      begin
-                          {ok, TreeId} = docterl_ets:new_tree([]),
-                          {ok, _ObjId, _AreaSpec} = docterl_ets:add_obj(TreeId, {0.1, 0.1, 0.1}, {0.1, 0.1, 0.1})
-                      end)
-                     ] end }}.
+test_new_obj() -> 
+    Pos = {0.1, 0.1, 0.1},
+    Size = {0.1, 0.1, 0.1},
+    {ok, TreeId} = docterl_ets:new_tree([]),
+    {ok, ObjId, _AreaSpec} = docterl_ets:add_obj(TreeId, Pos, Size),
+    ?assertMatch({ObjId, _}, docterl_ets:get_obj(ObjId)).
+
 
 %% remove_obj_test_() -> 
 %%     { "remove an object",
